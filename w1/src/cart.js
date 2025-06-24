43a2b3d0664c6310c5bbf3ed83a78fdb8ee1e830
yjhoo98 <yjhoo98@gmail.com>
2025-06-24 17:34:28 +0900

    day02

---
+++ w1/src/cart.js
@@ -0,0 +1,47 @@
+
+let cart = []
+
+export function addCart(menu) {
+
+    console.log("--------------------1")
+    console.log(menu)
+    
+    const targetMenuArr =cart.filter(obj => obj.mno === menu.mno)
+    console.log("--------------------2")
+    console.log(targetMenuArr)
+
+    if(targetMenuArr.length !==0){
+        console.log("--------------------3-1")
+        
+        targetMenuArr[0].qty = targetMenuArr[0].qty +1
+    }else {
+        console.log("--------------------3-2")
+        menu.qty = 1
+        cart = [...cart, menu]
+    }
+    
+    console.log(cart)
+    
+    return cart
+
+}
+export function changeCartQty(mno,oper) {
+    console.log("changeCartQty")
+
+    const targetMenu =cart.filter(obj => obj.mno === Number(mno))[0]
+
+    if(oper==='plus'){
+        targetMenu.qty+=1
+    }
+    else if(oper==='minus'){
+        if(targetMenu.qty===1){
+            cart=cart.filter(obj=>obj.mno!==Number(mno))
+        }
+        else{
+            targetMenu.qty-=1
+        }
+        
+    }
+    return cart
+
+}
